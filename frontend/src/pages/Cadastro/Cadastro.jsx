fbd22e90676fd52f60bf31432c18bf53b3728e29
lumahloi <115045300+lumahloi@users.noreply.github.com>
2024-08-20 16:14:04 -0300

    add: botão olho e validação dos inputs de cadastro

--- frontend/src/pages/Cadastro/Cadastro.jsx
+++ frontend/src/pages/Cadastro/Cadastro.jsx
@@ -5,7 +5,7 @@ import api from '../../services/api'
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 
-import { FiArrowLeft } from 'react-icons/fi'
+import { FiArrowLeft, FiEye, FiEyeOff } from 'react-icons/fi'
 
 const Cadastro = () => {
   const [name, setName] = useState('')
@@ -31,6 +31,9 @@ const Cadastro = () => {
     }
   }
 
+  function handleShowPass(method) {
+  }
+
   return (
     <div>
         <Header />
@@ -40,14 +43,18 @@ const Cadastro = () => {
 
             <form onSubmit={handleRegister}>
                 <label htmlFor="username">Nome de usuário</label>
-                <input type="text" name="username" id="username" placeholder="Esse será o nome exibido" value={name} onChange={e => setName(e.target.value)} maxLength={15} minLength={5}/>
+                <input type="text" name="username" id="username" placeholder="Esse será o nome exibido" value={name} onChange={e => setName(e.target.value)} maxLength={20} minLength={5}/>
 
                 <label htmlFor="useremail">E-mail</label>
-                <input type="email" name="useremail" id="useremail" placeholder="Insira seu melhor email" value={email} onChange={e => setEmail(e.target.value)}/>
+                <input type="email" name="useremail" id="useremail" placeholder="Insira seu melhor email" value={email} onChange={e => setEmail(e.target.value)} maxLength={30}/>
 
                 <label htmlFor="userpass">Senha</label>
-                <input type="password" name="userpass" id="userpass" value={password} onChange={e => setPassword(e.target.value)} minLength={8}/>
-
+                <div className="input-group" style={{position: 'relative'}}>
+                  <input type="password" name="userpass" id="userpass" value={password} onChange={e => setPassword(e.target.value)} minLength={8} maxLength={20}/>
+                  <button style={{backgroundColor:'transparent', width: '0px', height: '0px', position: 'absolute', top: '-15px', right: '40px', zIndex: '10', outline: 'none'}}>
+                    <FiEye size={25} color="#505F93"/>
+                  </button>
+                </div>
                 
                 <button>Continuar</button>
 
