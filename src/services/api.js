41db01920c261ff60c930d4283bbd99570ca89e1
lumahloi <115045300+lumahloi@users.noreply.github.com>
2024-09-04 20:17:56 -0300

    first commit

--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 
 const api = axios.create({
     //RAILWAY
-    baseURL: 'https://social-mana-api-production.up.railway.app/',
+    //baseURL: 'https://social-mana-api-production.up.railway.app/',
     //LOCALHOST
-    //baseURL: 'http://localhost:3333/'
+    baseURL: 'http://localhost:3333/'
 })
 
 export default api
